88e5b4d8ed1c6c1473d413188080d4619f7099b6
Nurleyanti <nsakidin@avans.nl>
2017-02-16 11:48:24 +0100

    over pagina

--- website/over.php
+++ website/over.php
@@ -1,9 +1,38 @@
-<?php
-require 'header.php';
-?>
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Over ons</title>
+            <?php
+            require 'header.php';
+            ?>
+        </head>
+        <body>
 
-<p>Dit is de over pagina</p>
 
+
+            <div class="container">
+                <h1 class="orange-title">Over ons</h1>
+                <h4>Doelstellingen Grafisch Atelier Den Bosch</h4>
+                <ol>
+                    <li>Het bieden van grafische werkplaatsfaciliteiten voor kunstenaars</li>
+                    <li>Educatie en kennisoverdracht</li>
+                    <li>Aanbieden van exposities en projecten met een relatie tot grafische kunst en vormgeving</li>
+                </ol>
+                <p><strong>Coördinator:</strong> Kristel van Genugten<br>
+                    <strong>Algemeen werkplaatsbeheer:</strong> Peter Koene<br>
+                    <strong>Beheer zeefdrukafdeling:</strong> Maartje van der Kruijs<br>
+                    <strong>Projectmedewerker Educatie:</strong> Nico Thöne<br>
+                    <br>
+                    <strong>Bestuursleden:</strong><br>
+                    - Jan Ramaekers – voorzitter<br>
+                    - Hans Koppens – penningmeester<br>
+                    - Gerard Pels – bestuurslid<br>
+                    - Roos Terra – bestuurslid<br>
+                    - Peter Korsman – bestuurslid<br>
+                    - Hans Derks – bestuurslid</p>
+            </div>
+        </body>
+    </html>
 <?php
 require 'footer.php';
 ?>
