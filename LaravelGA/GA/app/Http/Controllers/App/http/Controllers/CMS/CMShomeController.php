c05679dff88d73cfa021b0487e3d7815670667e9
Tim van Zal <tzal@avans.nl>
2017-02-21 21:22:29 +0100

    make controller

---
+++ LaravelGA/GA/app/Http/Controllers/App/http/Controllers/CMS/CMShomeController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\App\http\Controllers\CMS;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CMShomeController extends Controller
+{
+    //
+}
