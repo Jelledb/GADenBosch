c301bc524e46a104c7427bc832ee13d3fa7091d2
jorik jeeninga <jorik jeeninga>
2017-02-23 15:45:28 +0100

    made a new project, vendor map was missing

--- website/nieuws.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require 'header.php';
-?>
-
-<p>Dit is de nieuws pagina</p>
-
-<?php
-    require 'footer.php';
-?>
--- website/over.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'header.php';
-?>
-
-<p>Dit is de over pagina</p>
-
-<?php
-require 'footer.php';
-?>
