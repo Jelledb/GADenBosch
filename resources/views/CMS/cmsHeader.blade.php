feb9bd120ec1513f0cb8863c9a7a5c48b03f8d41
Tim van Zal <tzal@avans.nl>
2017-02-24 10:05:10 +0100

    Vendor

---
+++ resources/views/CMS/cmsHeader.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!-- Onze eigen CSS voor de website -->
+    <!--<link rel="stylesheet" href="website_style.css">
+    <!-- Onze eigen CSS voor CMS website -->
+    <link rel="stylesheet" href="../../public/css/cms_style.css" type="text/css">
+
+    <!-- Bootstrap CSS voor makkelijke responsiveness -->
+    <!-- build:css -->
+    <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.css">
+    <!-- endbuild -->
+
+    <!-- Font Awesome voor iconen -->
+    <!-- build:fa -->
+    <!--<link rel="stylesheet" href="../node_modules/font-awesome/css/font-awesome.min.css">
+    <!-- endbuild -->
+
+    <!-- Bootstrap settings -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+</head>
+<body>
+<header class="headers">
+  <div class="row">
+            <div class="col-md-2">
+                <h2 class="titel">Grafisch Atelier</h2>
+
+            </div>
+            <div class="col-md-8">
+
+            </div>
+            <div class="col-md-2">
+
+            </div>
+        </div>
+    <div class="row">
+        <div class="col-md-2">
+            <h3 class="cmsName">CMS</h3>
+        </div>
+        <div class="col-md-8">
+
+        </div>
+        <div class="col-md-2">
+
+        </div>
+    </div>
+</header>
+
