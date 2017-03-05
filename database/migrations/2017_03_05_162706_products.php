acfa3bd04af85ecaaaa52c06cfb7393a755e1162
jorik jeeninga <jorik jeeninga>
2017-03-05 17:35:25 +0100

    added migrations

---
+++ database/migrations/2017_03_05_162706_products.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Products extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories',function(Blueprint $table){
+            $table->increments('id');
+            $table->string('name');
+        });
+
+
+        Schema::create('products',function(Blueprint $table){
+            $table->increments('id');
+            $table->string('name');
+            $table->string('discriptionshort');
+            $table->string('discriptionlong');
+            $table->binary("image")->nullable();
+            $table->unsignedInteger('cat_id')->nullable();
+
+            $table->foreign('cat_id')->references('id')->on('categories');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
