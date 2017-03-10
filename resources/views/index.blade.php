d591fdfdb3a19f5a66ff646e3b0dcbb45a3bd9e2
StijnHendriks <sthendri@avans.nl>
2017-03-10 10:03:57 +0100

    Borders enzo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,13 +6,29 @@
     <div class="slider">
         <ul class="bxslider">
             <li><img src="{{asset('images/hp_home1.jpg')}}" title="foto 1"/></li>
-            <!--<li><img src="{{asset('images/hp_home2.jpg')}}" title="foto 2"/></li>
+            <li><img src="{{asset('images/hp_home2.jpg')}}" title="foto 2"/></li>
             <li><img src="{{asset('images/hp_home5.jpg')}}" title="foto 3"/></li>
-            <li><img src="{{asset('images/hp_werkplaatsfoto web 4.jpg')}}" title="foto 4"/></li>-->
+            <li><img src="{{asset('images/hp_werkplaatsfoto web 4.jpg')}}" title="foto 4"/></li>
         </ul>
     </div>
     <div class="nieuws-overzicht-home">
-        <div id="item" style="position: absolute; left: 0px; top: 0px;">
+        <div id="item" class="nieuws-nu" style="position: absolute; left: 0px; top: 0px;">
+            <h2 class="arch">Francine Steegs - Zeefdrukken, gemengde techniek en olieverfschilderijen</h2>
+            <div class="fotoblok" style="width:296px; height:419.00444444444px;">
+                <img src="{{asset('images/foto-nieuws-homepage/253_een verte voorbij onze ogen_.jpg')}}" width="296" height="419.00444444444" border="0">
+            </div>
+            <div class="textblok">In de werkplaats van het Grafisch Atelier, op de eerste verdieping van de Willem II Fabriek, exposeert Francine Steegs in de maand april zeefdrukken op papier en werken op doek en...</div>
+            <div class="leesmeer arch">LEES VERDER &gt;</div>
+        </div>
+        <div id="item" class="nieuws-workshops" style="position: absolute; left: 405px; top: 0px;">
+            <h2 class="arch">Francine Steegs - Zeefdrukken, gemengde techniek en olieverfschilderijen</h2>
+            <div class="fotoblok" style="width:296px; height:419.00444444444px;">
+                <img src="{{asset('images/foto-nieuws-homepage/253_een verte voorbij onze ogen_.jpg')}}" width="296" height="419.00444444444" border="0">
+            </div>
+            <div class="textblok">In de werkplaats van het Grafisch Atelier, op de eerste verdieping van de Willem II Fabriek, exposeert Francine Steegs in de maand april zeefdrukken op papier en werken op doek en...</div>
+            <div class="leesmeer arch">LEES VERDER &gt;</div>
+        </div>
+        <div id="item" class="nieuws-archief" style="position: absolute; left: 810px; top: 0px;">
             <h2 class="arch">Francine Steegs - Zeefdrukken, gemengde techniek en olieverfschilderijen</h2>
             <div class="fotoblok" style="width:296px; height:419.00444444444px;">
                 <img src="{{asset('images/foto-nieuws-homepage/253_een verte voorbij onze ogen_.jpg')}}" width="296" height="419.00444444444" border="0">
