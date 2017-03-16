e44693eaff0e0a37b587cd52f7317f0040c1c16a
chGm <chman@avans.nl>
2017-03-16 09:49:49 +0100

    isfriend isfriend, lol wut

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','isfriend','isfriend','role_id'
+        'name', 'email', 'password','isfriend','role_id'
     ];
 
     /**
