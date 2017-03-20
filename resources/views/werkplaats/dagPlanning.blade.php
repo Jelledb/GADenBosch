de398fc5c5a3a5b068d70668704562ad094b8d7e
jorik jeeninga <jorik jeeninga>
2017-03-20 19:19:27 +0100

    dagplanning in progres

--- resources/views/werkplaats/dagPlanning.blade.php
+++ resources/views/werkplaats/dagPlanning.blade.php
@@ -1,7 +1,7 @@
 
 <html>
 <head>
-
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
 </head>
 <body>
 
@@ -19,9 +19,21 @@
         for ($x = 0; $x <= 8; $x++) {
 
             $time = date('H:i', strtotime( $time .'+1 hour'));
-           echo '<input type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false" autocomplete="off" value="'.$time.'">
+//           echo '<input  id="'.$x.'" name="'.$x.'" type="button" class="btn btn-primary" data-toggle="button" aria-pressed="true" autocomplete="off" value="'.$time.'">
+//
+//            </input>';
+         echo  '<div class="btn-group" data-toggle="buttons">
+  <label class="btn btn-primary active">
+    <input type="checkbox" autocomplete="off" checked> Checkbox 1 (pre-checked)
+            </label>
+  <label class="btn btn-primary">
+    <input type="checkbox" autocomplete="off"> Checkbox 2
+            </label>
+  <label class="btn btn-primary">
+    <input type="checkbox" autocomplete="off"> Checkbox 3
+            </label>
+</div>';
 
-            </input>';
            // echo "<li> $time </li>";
 
         }
