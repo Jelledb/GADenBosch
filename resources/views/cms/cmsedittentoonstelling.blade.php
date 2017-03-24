0b8a655e9e005c6c643469e71198375bf186b3cb
Stijn Hendriks <sthendri@avans.nl>
2017-03-24 20:59:53 +0100

    More edits to cms

--- resources/views/cms/cmsedittentoonstelling.blade.php
+++ resources/views/cms/cmsedittentoonstelling.blade.php
@@ -7,37 +7,43 @@
         <div class="col-md-12 breadcrumb breadcrumbs"></div>
     </div>
     <div class="row">
-        <div class="col-md-9">
-            <h2>{{ $expo->title }}</h2>
-        </div>
-        <div class="col-md-3">
-
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-md-2">
-            <h4>Datum van</h4><br />
-            <input value="{{ $expo->date_from }}" />
-        </div>
-        <div class="col-md-1"></div>
-        <div class="col-md-2">
-            <h4>Datum tot</h4><br />
-            <input value="{{ $expo->date_to }}" />
+        <div class="col-md-8">
+            <h4>Titel</h4><br />
+            <input value="{{ $expo->title }}" />
         </div>
     </div>
 
     <br />
     <br />
-
     <div class="row">
         <div class="col-md-8">
-            <h4>Openingstijden</h4><br />
-            <input style="width: 300px;" value="{{ $expo->open_times }}" />
+            <h4>Zichtbaar</h4>
+            @if($expo->visible == 1)
+                <input type="checkbox" value="Visibility" name="visibility" checked>
+            @else
+                <input type="checkbox" value="Visibility" name="visibility">
+            @endif
         </div>
     </div>
 
     <br />
+    <br />
+    <div class="row">
+        <div class="col-md-8">
+            <h4>Subtitel</h4>
+            <input value="{{ $expo->subtitle }}" />
+        </div>
+    </div>
+
+    <br />
+    <br />
+    <div class="row">
+        <div class="col-md-8">
+            <h4>Introductie</h4>
+            <textarea rows="5" cols="80">{!! $expo->introduction !!}</textarea>
+        </div>
+    </div>
+
     <br />
     <br />
     <div class="row">
@@ -48,8 +54,96 @@
         <div class="col-md-4"></div>
     </div>
 
+    <br />
+    <br />
     <div class="row">
+        <div class="col-md-2">
+            <h4>Datum van</h4>
+            <input value="{{ $expo->date_from }}" />
+        </div>
+        <div class="col-md-1"></div>
+        <div class="col-md-2">
+            <h4>Datum tot</h4>
+            <input value="{{ $expo->date_to }}" />
+        </div>
+    </div>
 
+    <br />
+    <br />
+    <div class="row">
+        <div class="col-md-8">
+            <h4>Locatie</h4>
+            <select>
+                @if($expo->location == 'exporuimte')
+                    <option value="exporuimte" selected>Exporuimte</option>
+                    <option value="portiersloge">Portiersloge</option>
+                @else
+                    <option value="exporuimte">Exporuimte</option>
+                    <option value="portiersloge" selected>Portiersloge</option>
+                @endif
+            </select>
+        </div>
+    </div>
+
+    <br />
+    <br />
+    <div class="row">
+        <div class="col-md-8">
+            <h4>Locatie</h4>
+            <select>
+                @if($expo->toon == 'yes')
+                    <option value="yes" selected>Ja</option>
+                    <option value="no">Nee</option>
+                @else
+                    <option value="yes">Ja</option>
+                    <option value="no" selected>Nee</option>
+                @endif
+            </select>
+        </div>
+    </div>
+
+    <br />
+    <br />
+    <div class="row">
+        <div class="col-md-8">
+            <h4>Openingstijden</h4>
+            <input style="width: 300px;" value="{{ $expo->open_times }}" />
+        </div>
+    </div>
+
+    <br />
+    <br />
+    <div class="row">
+        <div class="col-md-8">
+            <h4>Extra</h4>
+            <input style="width: 300px;" value="{{ $expo->extra }}" />
+        </div>
+    </div>
+
+    <br />
+    <br />
+    <div class="row">
+        <div class="col-md-2">
+            <h4>Opening</h4>
+            <input value="{{ $expo->opening }}" />
+        </div>
+        <div class="col-md-1"></div>
+        <div class="col-md-2">
+            <h4>Opening</h4>
+            <input value="{{ $expo->opening_time }}" />
+        </div>
+    </div>
+
+    <br />
+    <br />
+    <div class="row">
+        <div class="col-md-8">
+            <h4>Melding</h4>
+            <input value="{{ $expo->message }}" />
+        </div>
+    </div>
+
+    <div class="row">
         <div class="col-md-3">
             Hier komt de tekst in 2 kolommen:
             <div class="radio text-center">
