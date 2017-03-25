21f25ae06f01099472c7db65179356b642516adb
Nurleyanti <nsakidin@avans.nl>
2017-03-25 22:38:43 +0100

    mollie

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        Mollie\Laravel\MollieServiceProvider::class,
 
 
         /*
@@ -228,7 +229,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
-        'Html' => Collective\Html\HtmlFacade::class
+        'Html' => Collective\Html\HtmlFacade::class,
+        'Mollie' => Mollie\Laravel\Facades\Mollie::class,
 
     ],
 
