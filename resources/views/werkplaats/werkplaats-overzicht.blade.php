cd93682a5dfbf5d696deda4f495c443a6c669437
chGm <chman@avans.nl>
2017-04-10 01:13:31 +0200

    Werkplaats afbeelding toegevoegd. De lijst werkplaatsen in het midden gezet.

--- resources/views/werkplaats/werkplaats-overzicht.blade.php
+++ resources/views/werkplaats/werkplaats-overzicht.blade.php
@@ -18,16 +18,22 @@
                     <h1 class="centerH1">Werkplaatsen</h1>
                     <br/>
                 </div>
-                <ul>
-                    @foreach($workplaces as $workplace)
+                <div class="workspaces">
+                    <ul>
+                        @foreach($workplaces as $workplace)
 
-                        <li>
-                            <a href = "{{url("detailed-werkplaats/".$workplace->id)}}"> {{ $workplace->name . " " . $workplace->size}}</a>
-                        </li>
+                            <li>
+                                <a href="{{url("detailed-werkplaats/".$workplace->id)}}">{{$workplace->name . " " . $workplace->size}}</a>
+                            </li>
 
-                    @endforeach
+                        @endforeach
 
-                </ul>
+                    </ul>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                {{--{!! Html::image(asset('images/werkplaats-overzicht.png')) !!}}--}}
+                <img src="{{asset('images/werkplaats-overzicht.png')}}" alt="image" width="750px" height="400px">
             </div>
         </div>
     </div>
