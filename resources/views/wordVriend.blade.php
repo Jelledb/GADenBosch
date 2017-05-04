555d190cfc89ac40635215828b76a9de179e5ea1
Nurleyanti <nsakidin@avans.nl>
2017-05-04 15:22:07 +0200

    message test

--- resources/views/wordVriend.blade.php
+++ resources/views/wordVriend.blade.php
@@ -11,7 +11,7 @@
                     <br>
                     <div class="text">
 
-                        Oook in 2017 kunnen wij niet zonder onze vrienden.<br>
+                        Ook in 2017 kunnen wij niet zonder onze vrienden.<br>
                         Met een kleine of grote vriendendonatie helpt u het Grafisch Atelier ook in 2017 weer aan de weg te timmeren. We hebben weer veel mooie plannen voor projecten, workshops en exposities.
                         Vrienden ontvangen in 2017 een grafisch vriendengeschenk van Alexia Pnevmatikos gratis!
                         <br><br>
