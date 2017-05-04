ce4c48da96e20c282dead2a434459c859c10619a
Nurleyanti <nsakidin@avans.nl>
2017-05-04 17:42:08 +0200

    show message layout

--- resources/views/wordVriend.blade.php
+++ resources/views/wordVriend.blade.php
@@ -3,20 +3,31 @@
 @section('content')
 
     <div class="container">
+        @if(session('success'))
+            <div class="alert alert-success">Betaling is gelukt</div>
+        @elseif(session('fail'))
+            <div class="alert alert-warning">Betaling is mislukt</div>
+        @endif
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
+
                     <h1 class="centerH1">Vriend worden?</h1>
 
 
                     <div class="text">
 
-                        Oook in 2017 kunnen wij niet zonder onze vrienden.<br>
+                        Ook in 2017 kunnen wij niet zonder onze vrienden.<br>
                         Met een kleine of grote vriendendonatie helpt u het Grafisch Atelier ook in 2017 weer aan de weg te timmeren. We hebben weer veel mooie plannen voor projecten, workshops en exposities.
                         Vrienden ontvangen in 2017 een grafisch vriendengeschenk van Alexia Pnevmatikos gratis!
                         <br><br>
                         Vriend van het GA ben je al vanaf € 45,- per jaar, maar een groter bedrag is meer dan welkom.
                         <br>
+                        @if(session('success'))
+                            <div class="alert alert-success">Betaling is gelukt</div>
+                        @elseif(session('fail'))
+                            <div class="alert alert-warning">Betaling is mislukt</div>
+                        @endif
 
                         <div class="wrapper-btn">
                             @if (Auth::check() && Auth::user()->isfriend == 1)
@@ -24,11 +35,7 @@
                         @else
                                 <button type="button" class="btn btn-default" id="vriend-btn"><a id="vriend-a" href="/vriend-worden-pay">Ja, ik word vriend!</a></button>
                             @endif
-                                @if(session('success'))
-                                    <div class="alert alert-success">Betaling is gelukt</div>
-                                @elseif(session('fail'))
-                                    <div class="alert alert-warning">Betaling is mislukt</div>
-                                @endif
+
                         </div>
                     </div>
                 </div>
