05e98a7749de379ffe7703a2798fa12218cea922
chGm <chgm_@hotmail.com>
2017-05-06 04:19:51 +0200

    First set of business rules set up.

--- app/Http/Controllers/WorkplaceController.php
+++ app/Http/Controllers/WorkplaceController.php
@@ -7,6 +7,7 @@ use App\reservation_workspace;
 use App\Workspace;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
 
 class WorkplaceController extends Controller
 {
@@ -29,7 +30,7 @@ class WorkplaceController extends Controller
 
     function getDayplanning($currentDay, $id)
     {
-        $data['occupation'] = reservation_workspace::Occupationday($id, $currentDay)->get();;
+        $data['occupation'] = reservation_workspace::Occupationday($id, $currentDay)->get();
         $data['day'] = $currentDay;
         $data['id'] = $id;
         $data['workspace'] = Workspace::findOrFail($id);
@@ -50,11 +51,30 @@ class WorkplaceController extends Controller
 
     function createReservation(Request $request)
     {
-
         $start = $request->dag . " " . $request->start . ":00";
         $end = $request->dag . " " . $request->end . ":00";
         // TODO AUTH
         // $user = $request->gebruiker;
+
+        $reserveringen = reservation_workspace::Occupationday($request->werkplaats, $request->dag)->get();;
+
+        if(isset($reserveringen)) {
+            foreach ($reserveringen as $reservering) {
+
+                if($start < $reservering->date_in && $end < $reservering->date_in) {
+                    return redirect()->back()->with(session()->flash('reservationBAD', 'Reserveren is mislukt!'));
+                }
+                if($start < $reservering->date_out && $end < $reservering->date_out) {
+                    return redirect()->back()->with(session()->flash('reservationBAD', 'Reserveren is mislukt!'));
+                }
+                if($start < $reservering->date_in && $end > $reservering->date_in) {
+                    return redirect()->back()->with(session()->flash('reservationBAD', 'Reserveren is mislukt!'));
+                }
+                if($start > $reservering->date_in && $start < $reservering->date_out) {
+                    return redirect()->back()->with(session()->flash('reservationBAD', 'Reserveren is mislukt!'));
+                }
+            }
+        }
 
         $reservation = new Reservation();
         $reservation->date_in = $start;
@@ -67,7 +87,7 @@ class WorkplaceController extends Controller
 
         reservation_workspace::create(['reservation_id' => $id, 'workspace_id' => $request->werkplaats]);
 
-        return redirect('werkplaats-overzicht');
+        return redirect()->back()->with(session()->flash('reservationOK', 'Reserveren is gelukt!!'));
     }
 
     function myReservations()
