f373025ebdfc328816e803effb32e4f4f8969e93
Jelle den Burger <jelledenburger@icloud.com>
2017-05-07 22:22:23 +0200

    Titel en beschrijvingen CMS werkt nu.

--- app/RouteMetaTag.php
+++ app/RouteMetaTag.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class RouteMetaTag extends Model
 {
-    //
+    public $timestamps = false;
+    protected $fillable = ['id', 'route', 'title', 'description'];
 }
