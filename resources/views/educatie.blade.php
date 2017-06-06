40cddf428e49d4b515c04dfcd8f6439e533e0888
Tim van Zal <timvanzal@hotmail.com>
2017-06-06 16:08:03 +0200

    educatie fix

--- resources/views/educatie.blade.php
+++ resources/views/educatie.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.website')
 @section('content')
     <div class="container">
+        @if($page != null)
         <h2 class="col-md-offset-2">{!! $page->title !!}</h2>
         <br><br>
         <div class="row">
@@ -11,7 +12,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-4" >
+            <div class="col-md-4">
                 <div class="panel panel-default">
                     <div class="over-content">
                         {!! $page->content_right !!}
@@ -19,5 +20,20 @@
                 </div>
             </div>
         </div>
+        @else
+            <div class="row">
+                <div class="col-md-3 col-md-offset-2"></div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 col-md-offset-3">
+                    <div class="panel panel-default">
+                        <p class="no-item">Sorry er zijn geen educatie pagina's</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3 col-md-offset-2"></div>
+            </div>
+            @endif
     </div>
 @endsection
